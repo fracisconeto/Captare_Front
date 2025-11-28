b24a6d57937c008b2e623ab1a38cc79e22ab2cbc
larissalotin <larissalt1234@gmail.com>
2025-11-28 14:15:04 -0300

    feat: atualizar título da aplicação para "Captare" e corrigir versão do cache

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hj3gl89abj8"
+    "revision": "0.9v93svbsqi"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
